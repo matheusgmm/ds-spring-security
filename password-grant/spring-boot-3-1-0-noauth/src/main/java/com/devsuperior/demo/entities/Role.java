44e933a5ac59b6cc13bb9bbfa1b9717cf5655248
Matheus Muccio <matheus.muccio@rodojacto.com.br>
2024-05-16 07:47:38 -0300

    Spring Security checklist, not tested

--- password-grant/spring-boot-3-1-0-noauth/src/main/java/com/devsuperior/demo/entities/Role.java
+++ password-grant/spring-boot-3-1-0-noauth/src/main/java/com/devsuperior/demo/entities/Role.java
@@ -1,11 +1,12 @@
 package com.devsuperior.demo.entities;
 
 import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
 
 import java.util.Objects;
 @Entity
 @Table(name = "tb_role")
-public class Role {
+public class Role implements GrantedAuthority {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -15,11 +16,12 @@ public class Role {
     public Role() {
     }
 
-    public Role(String authority, Long id) {
-        this.authority = authority;
+    public Role(Long id, String authority) {
         this.id = id;
+        this.authority = authority;
     }
 
+    @Override
     public String getAuthority() {
         return authority;
     }
